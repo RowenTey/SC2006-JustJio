f3207bb31ee6d53e474ad54aa7a3f8dff4bcf1bf
RowenTey <kaiseong02@gmail.com>
2022-09-07 11:09:51 +0800

    KaiSeong test commit

--- client/App.js
+++ client/App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Login from './login';
 import type {Node} from 'react';
 import {
@@ -18,7 +18,6 @@ import {
   useColorScheme,
   View,
   TouchableOpacity,
-
 } from 'react-native';
 
 import {
@@ -30,70 +29,39 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 const App = () => {
   return (
-
     <>
-      <View style = {Styles.container}>
-        <Login/>
+      <View style={Styles.container}>
+        <Login />
       </View>
-
-      
     </>
-
-
-
-
-
-
-  )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
-
+  );
+};
 
 const Styles = StyleSheet.create({
   header: {
-    width : '100%' ,
-    height : '15%',
-    alignItems : 'center',
-    justifyContent : 'center',
-    backgroundColor : '#E9D7FD'
-
+    width: '100%',
+    height: '15%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#E9D7FD',
   },
 
-  head : {
-    backgroundColor : '#E9D7FD',
+  head: {
+    backgroundColor: '#E9D7FD',
   },
 
-  container : {
-    flex : 1,
-    alignItems : 'center',
-    justifyContent : 'center',
-    backgroundColor : '#E9D7FD'
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#E9D7FD',
   },
 
   text: {
-    fontSize : 80,
-   // changed from 56 to 80 on fontsize
-    fontFamily : "OleoScript-Bold",
-    color : "#4E1164",
-    
-  }
+    fontSize: 56,
+    fontFamily: 'OleoScript-Bold',
+    color: '#4E1164',
+  },
 });
-
-
 
 export default App;
