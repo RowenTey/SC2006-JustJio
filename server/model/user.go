851371e022b114c42225d459ccf404fe8fffaa46
RowenTey <kaiseong02@gmail.com>
2022-09-14 23:51:04 +0800

    Update phoneNum default value

--- server/model/user.go
+++ server/model/user.go
@@ -8,7 +8,7 @@ type User struct {
 	Email          string         `gorm:"unique; not null" json:"email"`
 	Password       string         `gorm:"not null" json:"password"`
 	Name           string         `json:"name"`
-	Phone_Num      int            `json:"phoneNum"`
+	Phone_Num      int            `gorm:"default:null" json:"phoneNum"`
 	RegisteredAt   int64          `gorm:"autoCreateTime" json:"-"`
 	UpdatedAt      int64          `gorm:"autoUpdateTime:milli" json:"-"`
 	AttendingRooms datatypes.JSON `json:"attendingRooms"`
