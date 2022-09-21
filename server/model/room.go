be9e0aafe6fc77af84d3c3eddb4fbd53f2eab23b
RowenTey <kaiseong02@gmail.com>
2022-09-21 15:07:53 +0800

    Update backend models

--- server/model/room.go
+++ server/model/room.go
@@ -8,7 +8,7 @@ import (
 
 type Room struct {
 	ID             uint           `gorm:"primaryKey"`
-	Name           string         `gorm:"unique; not null" json:"name"`
+	Name           string         `gorm:"not null" json:"name"`
 	Time           string         `gorm:"not null" json:"time"`
 	Venue          string         `gorm:"not null" json:"venue"`
 	Host           string         `gorm:"not null" json:"host"`
@@ -16,7 +16,15 @@ type Room struct {
 	URL            string         `json:"url"`
 	CreatedAt      time.Time      `gorm:"autoCreateTime" json:"CreatedAt"`
 	UpdatedAt      time.Time      `gorm:"autoUpdateTime" json:"-"`
-	Attendees      datatypes.JSON `json:"attendees"`
 	PartyItems     datatypes.JSON `json:"partyItems"`
 	// Date           datatypes.Date `json:"date"`
+}
+
+// juntion table to model user & room relationships
+type RoomUser struct {
+	ID         uint `gorm:"primaryKey"`
+	UserID     uint `gorm:"not null" json:"userID"`
+	RoomID     uint `gorm:"not null" json:"RoomID"`
+	IsHost     bool `gorm:"default:false" json:"isHost"`
+	IsAttendee bool `gorm:"default:false" json:"IsAttendee"`
 }
