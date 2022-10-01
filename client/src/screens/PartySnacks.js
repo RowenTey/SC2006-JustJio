bfea4b7d41885c0d34b8c237a4e8129cddb6a5e1
amabel <amabel0110@gmail.com>
2022-10-01 14:10:06 +0800

    added images

--- client/src/screens/PartySnacks.js
+++ client/src/screens/PartySnacks.js
@@ -7,6 +7,7 @@ import {
   TouchableOpacity,
   TextInput,
   Image,
+  ScrollView,
 } from 'react-native';
 
 const PartySnacks = () => {
@@ -18,43 +19,88 @@ const PartySnacks = () => {
         <Text style={Styles.smalltext}>Current location: Bukit Batok Street 21</Text>
       </View>
 
-      <TouchableOpacity style={Styles.box}>
-        <Text style={Styles.details}>Sheng Shiong Commonwealth</Text>
-        <Text style={Styles.details}>5 km</Text>
-        <Text style={Styles.details}>$</Text>
-      </TouchableOpacity>
+      <ScrollView style={Styles.middle}>
+        <TouchableOpacity style={Styles.box}>
+          <Image style={Styles.images}
+            source={{
+              width: 90,
+              height: 65,
+              uri: "https://mustsharenews.com/wp-content/uploads/2022/03/sheng-siong.png" }} />
+          <View style={Styles.textcontainer}>
+            <Text style={Styles.details}>Sheng Shiong Commonwealth</Text>
+            <Text style={Styles.details}>5 km</Text>
+            <Text style={Styles.details}>$</Text>
+          </View>
+        </TouchableOpacity>
 
-      <TouchableOpacity style={Styles.box}>
-        <Text style={Styles.details}>Fairprice Stirling Road</Text>
-        <Text style={Styles.details}>2.7 km</Text>
-        <Text style={Styles.details}>$$</Text>
-      </TouchableOpacity>
+        <TouchableOpacity style={Styles.box}>
+          <Image style={Styles.images}
+            source={{
+              width: 90,
+              height: 65,
+              uri: "https://lh5.googleusercontent.com/p/AF1QipPFW2cMfqIoguGFjtKpBLWpIV3fVtPRGlICFIpd=w408-h306-k-no" }} />
+          <View style={Styles.textcontainer}>
+            <Text style={Styles.details}>Fairprice Stirling Road</Text>
+            <Text style={Styles.details}>2.7 km</Text>
+            <Text style={Styles.details}>$$</Text>
+          </View>
+        </TouchableOpacity>
 
-      <TouchableOpacity style={Styles.box}>
-        <Text style={Styles.details}>Fairprice Dawson Road</Text>
-        <Text style={Styles.details}>4.9 km</Text>
-        <Text style={Styles.details}>$$</Text>
-      </TouchableOpacity>
+        <TouchableOpacity style={Styles.box}>
+          <Image style={Styles.images}
+            source={{
+              width: 90,
+              height: 65,
+              uri: "https://lh3.googleusercontent.com/p/AF1QipPtdWDY8T0YiekdvFFj67pPASt_S1YwVtTvsOEw=s1360-w1360-h1020" }} />
+          <View style={Styles.textcontainer}>
+            <Text style={Styles.details}>Fairprice Dawson Road</Text>
+            <Text style={Styles.details}>4.9 km</Text>
+            <Text style={Styles.details}>$$</Text>
+          </View>
+        </TouchableOpacity>
 
-      <TouchableOpacity style={Styles.box}>
-        <Text style={Styles.details}>Cold Storage Holland Village</Text>
-        <Text style={Styles.details}>3.1 km</Text>
-        <Text style={Styles.details}>$$$</Text>
-      </TouchableOpacity>
+        <TouchableOpacity style={Styles.box}>
+          <Image style={Styles.images}
+            source={{
+              width: 90,
+              height: 65,
+              uri: "https://shopsinsg.com/wp-content/uploads/2016/06/cold-storage-stores-singapore.jpg" }} />
+          <View style={Styles.textcontainer}>
+            <Text style={Styles.details}>Cold Storage Holland Village</Text>
+            <Text style={Styles.details}>3.1 km</Text>
+            <Text style={Styles.details}>$$$</Text>
+          </View>
+        </TouchableOpacity>
 
-      <TouchableOpacity style={Styles.box}>
-        <Text style={Styles.details}>CS Fresh The Star Vista</Text>
-        <Text style={Styles.details}>1.4 km</Text>
-        <Text style={Styles.details}>$$$</Text>
-      </TouchableOpacity>
+        <TouchableOpacity style={Styles.box}>
+          <Image style={Styles.images}
+            source={{
+              width: 90,
+              height: 65,
+              uri: "https://d3ckgtbv0fk1sf.cloudfront.net/media/uploads/af48320d2b22356a0bfff0bb341ee74b.png" }} />
+          <View style={Styles.textcontainer}>
+            <Text style={Styles.details}>CS Fresh The Star Vista</Text>
+            <Text style={Styles.details}>1.4 km</Text>
+            <Text style={Styles.details}>$$$</Text>
+          </View>
+        </TouchableOpacity>
 
-      <TouchableOpacity style={Styles.box}>
-        <Text style={Styles.details}>Sheng Shiong Ghim Moh Link</Text>
-        <Text style={Styles.details}>1.2 km</Text>
-        <Text style={Styles.details}>$</Text>
-      </TouchableOpacity>
+        <TouchableOpacity style={Styles.box}>
+          <Image style={Styles.images}
+            source={{
+              width: 90,
+              height: 65,
+              uri: "https://nestia-food.obs.ap-southeast-3.myhuaweicloud.com/201609/26/69644666e0c6f231d630023e78d9c30a.jpg" }} />
+          <View style={Styles.textcontainer}>
+            <Text style={Styles.details}>Sheng Shiong Ghim Moh Link</Text>
+            <Text style={Styles.details}>1.2 km</Text>
+            <Text style={Styles.details}>$</Text>
+          </View>
+        </TouchableOpacity>
+      </ScrollView>
 
       <View style={Styles.bottombar}>
+        <Text style={Styles.smalltext}>Nav Bar</Text>
       </View>
 
     </View>
@@ -66,28 +112,48 @@ export default PartySnacks;
 const Styles = StyleSheet.create({
   box: {
     backgroundColor: "#FFFFFF",
-    justifyContent: "center",
+    justifyContent: "flex-start",
+    alignItems: "center",
     borderRadius: 15,
-    width: 340,
-    height: 80,
-    marginVertical: 5,
+    marginHorizontal: 75,
+    marginTop: 25,
+    flexDirection: 'row',
+    paddingVertical: 5,
+  },
+
+  textcontainer: {
+    flexDirection: 'column',
   },
 
   topbar: {
+    flex: 1,
+    flexDirection: 'column',
     backgroundColor: "#E9D7FD",
-    width: "100%",
     justifyContent: "center",
     alignItems: 'center',
-    paddingVertical: 20,
-    height: 80,
+    minHeight: '10%',
+    maxHeight: '10%',
+    width: '100%',
   },
 
   bottombar: {
+    flex: 3,
+    flexDirection: 'column',
     backgroundColor: "#E9D7FD",
-    width: "100%",
+    minHeight: '7%',
+    maxHeight: '7%',
     justifyContent: "center",
     alignItems: 'center',
-    height: 60,
+    width: '100%',
+  },
+
+  middle: {
+    flex: 2,
+    flexDirection: 'column',
+    backgroundColor: "#EEEEEE",
+    width: 500,
+    minHeight: '83%',
+    maxHeight: '83%',
   },
 
   details: {
@@ -107,15 +173,14 @@ const Styles = StyleSheet.create({
   },
 
   container: {
-    flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     backgroundColor: '#EEEEEE',
-    width: "100%",
+    height: "100%",
   },
 
   images: {
-    width: 100,
-    height: 100,
+    margin: 8,
+    borderRadius: 10,
   },
 });
