c83c81cb83c626319fd1488fe175cc19ebda93bd
czhi-heng <angus2998@hotmail.com>
2022-10-03 14:05:35 +0800

    Zh commit

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -1,6 +1,11 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+const ICONS = {
+  add: require('../../assets/images/add.png'),
+  mail: require('../../assets/images/mail.png'),
+};
 
 const Home = ({ navigation }) => {
   return (
@@ -8,22 +13,48 @@ const Home = ({ navigation }) => {
       <View style={styles.top}>
         <Text style={styles.header}>Welcome, user!</Text>
       </View>
-      <View>
-        <Text>TO GIVE:</Text>
-      </View>
-      <View>
-        <Text>TO GET:</Text>
-      </View>
-      <TouchableOpacity>
-        <Text onPress={() => navigation.navigate('Splash')}>Create Room</Text>
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <Text onPress={() => navigation.navigate('TransactionHistory')}>
-          Room Invitations
-        </Text>
-      </TouchableOpacity>
-      <View>
-        <Text>Party Rooms</Text>
+
+      <View style={styles.middle}>
+        <View style={styles.transactions}>
+          <View>
+            <Text>TO GIVE:</Text>
+          </View>
+          <View>
+            <Text>TO GET:</Text>
+          </View>
+        </View>
+
+        <View style={styles.roomFunctions}>
+          <TouchableOpacity onPress={() => navigation.navigate('Splash')}>
+            <View style={styles.roomFunctionButtons}>
+              <Image
+                source={ICONS.add}
+                style={{
+                  width: 30,
+                  height: 30,
+                }}
+              />
+            </View>
+            <Text style={styles.roomFunctionText}>Create Room</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('TransactionHistory')}>
+            <View style={[styles.roomFunctionButtons, { marginStart: 18 }]}>
+              <Image
+                source={ICONS.mail}
+                style={{
+                  width: 40,
+                  height: 30,
+                }}
+              />
+            </View>
+            <Text style={styles.roomFunctionText}>Room Invitations</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.rooms}>
+          <Text style={styles.roomsTitle}>Party Rooms</Text>
+        </View>
       </View>
     </View>
   );
@@ -41,8 +72,8 @@ const styles = StyleSheet.create({
 
   top: {
     flex: 1,
+    right: 60,
     flexDirection: 'column',
-    alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#E9D7FD',
     minHeight: '10%',
@@ -51,10 +82,63 @@ const styles = StyleSheet.create({
 
   header: {
     fontSize: 25,
-    top: 10,
+    bottom: 10,
     fontFamily: 'OleoScript-Bold',
+    color: '#4E1164',
+  },
+
+  middle: {
+    flex: 2,
+    flexDirection: 'column',
+    backgroundColor: '#f0ecec',
     alignItems: 'center',
-    justifyContent: 'center',
+    width: '100%',
+    maxHeight: '83%',
+  },
+
+  transactions: {
+    flexDirection: 'row',
+    borderColor: 'red',
+    borderWidth: 5,
+    width: '80%',
+    justifyContent: 'space-between',
+    padding: 20,
+    marginVertical: 20,
+  },
+
+  roomFunctions: {
+    flexDirection: 'row',
+    borderColor: 'red',
+    borderWidth: 5,
+    width: '80%',
+    justifyContent: 'space-between',
+    padding: 20,
+    paddingHorizontal: 30,
+    marginVertical: 20,
+  },
+
+  roomFunctionText: {
+    fontWeight: 'bold',
+  },
+
+  roomFunctionButtons: {
+    alignItems: 'center',
+    backgroundColor: '#4E1164',
+    padding: 15,
+    paddingVertical: 18,
+    borderRadius: 15,
+    width: 70,
+    marginStart: 6,
+    marginBottom: 5,
+  },
+
+  rooms: {
+    marginTop: 30,
+  },
+
+  roomsTitle: {
+    fontSize: 30,
+    fontWeight: 'bold',
     color: '#4E1164',
   },
 });
---
+++ client/src/screens/RoomsPage.js
@@ -0,0 +1,189 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const RoomsPage = () => {
+  const list1 = [
+    ['Friday', '26 Dec', "Graduation Party", "1800", "Bob's House", "22"],
+    ['Hilary', 'Marcus', 'Jane', 'Jeff', 'Letitia', 'Mark'],
+  ];
+  const list2 = [
+    ['ammaeudos', '2342', true],
+    ['dobin', '45', false],
+  ];
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.top}>
+        <Text style={styles.header}>6D</Text>
+      </View>
+
+      <View style={styles.middle}>
+        <Text style={styles.smallText}>Upcoming Event</Text>
+        <View style={styles.oneDate}>
+          <EventDetail list={list1} />
+        </View>
+        <Text style={styles.smallText}>Members</Text>
+        <GuestList list={list2} />
+      </View>
+
+      <View style={styles.bottom}>
+        <Text style={styles.smallText}>Nav Bar</Text>
+      </View>
+    </View>
+  );
+};
+
+const EventDetail = props => {
+  return (
+    <View>
+      <Text style={styles.date}>{props.date}</Text>
+      <View>
+        {props.list.map(listItem => (
+          <Box
+            key={listItem[0]}
+            day={listItem[0]}
+            date={listItem[1]}
+            eventName={listItem[2]}
+            time={listItem[3]}
+            venue={listItem[4]}
+            noOfParticpants={listItem[5]}
+          />
+        ))}
+      </View>
+    </View>
+  );
+};
+
+const GuestList = props => {
+  return (
+    <View>
+      <Text style={styles.date}>{props.date}</Text>
+      <View>
+        {props.list.map(listItem => (
+          <Box
+            key={listItem[0]}
+            name={listItem[0]}
+            amount={listItem[1]}
+            isReceive={listItem[2]}
+          />
+        ))}
+      </View>
+    </View>
+  );
+};
+
+const Box = props => {
+  let styleSheet = '';
+  if (props.isReceive) {
+    styleSheet = styles.amountReceive;
+  } else {
+    styleSheet = styles.amountGive;
+  }
+
+  return (
+    <View>
+      <View style={styles.oneDate}>
+        <Text style={styles.name}>{props.name}</Text>
+        <Text style={styleSheet}>{props.amount}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default RoomsPage;
+
+const styles = StyleSheet.create({
+  header: {
+    fontSize: 25,
+    top: 10,
+    fontFamily: 'OleoScript-Bold',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: '#4E1164',
+  },
+
+  head: {
+    backgroundColor: '#E9D7FD',
+  },
+
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#E9D7FD',
+    height: '100%',
+  },
+
+  top: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#E9D7FD',
+    minHeight: '10%',
+    maxHeight: '10%',
+  },
+
+  middle: {
+    flex: 2,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#f0ecec',
+    width: 500,
+    minHeight: '83%',
+    maxHeight: '83%',
+  },
+
+  bottom: {
+    flex: 3,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#E9D7FD',
+    minHeight: '7%',
+    maxHeight: '7%',
+  },
+
+  oneDate: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#E9D7FD',
+    width: 370,
+    height: 80,
+    borderRadius: 15,
+  },
+
+  date: {
+    fontSize: 15,
+    left: 3,
+    fontFamily: 'OleoScript-Bold',
+    color: '#4E1164',
+  },
+
+  name: {
+    fontSize: 20,
+    top: -5,
+    left: -110,
+    color: '#000000',
+  },
+
+  amountReceive: {
+    fontSize: 20,
+    top: 12,
+    left: 140,
+    color: '#00FF00',
+  },
+
+  amountGive: {
+    fontSize: 20,
+    top: 12,
+    left: 140,
+    color: '#FF0000',
+  },
+
+  smallText: {
+    fontSize: 13,
+    color: '#4E1164',
+    flexDirection: 'row',
+  },
+});
