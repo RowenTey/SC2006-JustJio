dfeaad65812da79dbd3559b5910b0dd99ac5fd29
Harish Vasanth <94796213+JULU909@users.noreply.github.com>
2022-10-22 13:41:16 +0800

    added more functions

--- client/src/context/transaction.js
+++ client/src/context/transaction.js
@@ -5,6 +5,89 @@ import {
   FETCH_TRANSACTION,
   CREATE_TRANSACTION,
   START_LOADING,
-} from '../constants/transactionActions';
+} from '../constants/actionTypes';
 
 import { AxiosContext } from './axios';
+import TransactionReducer, { initialTransactionState } from '../reducers/transactionReducers';
+
+const TransactionContext = createContext(null);
+const { Provider } = TransactionContext;
+
+
+
+const TransactionProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(TransactionReducer, initialTransactionState);
+    const { authAxios } = useContext(AxiosContext);
+  
+    const fetchTransactions = async () => {
+      try {
+        dispatch({
+          type: START_LOADING,
+        });
+        const { data: response } = await authAxios.get('/transactions');
+        dispatch({
+          type: FETCH_TRANSACTION,
+          payload: response,
+        });
+        dispatch({
+          type: END_LOADING,
+        });
+      } catch (error) {
+        console.log('Failed to fetch transactions', error);
+        if (error.response) {
+          console.log('Error response', error.response.data);
+        } else if (error.request) {
+          console.log('Error request', error.request);
+        }
+      }
+    };
+  
+    const createTransactions = async TransactionData => {
+      try {
+        dispatch({
+          type: START_LOADING,
+        });
+  
+        const { data: response } = await authAxios.post('/transactions', TransactionData);
+        const updatedTransactions = state.transactions.concat(response.data);
+        const updatedTotal = updatedTransactions.length;
+        dispatch({
+          type: CREATE_TRANSACTION,
+          payload: {
+            transactions: updatedTransactions,
+            total: updatedTotal,
+          },
+        });
+  
+        dispatch({
+          type: END_LOADING,
+        });
+      } catch (error) {
+        console.log('Failed to create transaction', error);
+        if (error.response) {
+          console.log('Error response', error.response.data);
+          if (error.response.data.message === "User doesn't exist") {
+            throw new Error("User doesn't exist");
+          }
+        } else if (error.request) {
+          console.log('Error request', error.request);
+        }
+      }
+    };
+  
+
+    
+  
+    const value = {
+      total: state.total,
+      transactions: state.transactions,
+      isLoading: state.isLoading,
+      createTransactions,
+      fetchTransactions,
+      
+    };
+  
+    return <Provider value={value}>{children}</Provider>;
+  };
+
+  export { TransactionContext, TransactionProvider };
--- client/src/reducers/transactionReducers.js
+++ client/src/reducers/transactionReducers.js
@@ -4,10 +4,45 @@ import {
   FETCH_TRANSACTION,
   CREATE_TRANSACTION,
   START_LOADING,
-} from '../constants/transactionActions';
+} from '../constants/actionTypes';
 
 export const initialTransactionState = {
     isLoading: false,
     total: 0,
     transactions: [],
   };
+
+const TransactionReducer = (state, action) => {
+    const { type, payload } = action;
+    switch (type) {
+        case START_LOADING:
+          return {
+            ...state,
+            isLoading: true,
+          };
+        case END_LOADING:
+          return {
+            ...state,
+            isLoading: false,
+          };
+        case FETCH_TRANSACTION:
+          return {
+            ...state,
+            transactions: payload.data,
+            total: payload.data.length,
+          };
+
+        case CREATE_TRANSACTION:
+            return {
+                ...state,
+                transactions: payload.rooms,
+                total: payload.total,
+              };
+        case LOGOUT:
+                return initialTransactionState;
+        default:
+                throw new Error(`No case for type ${type} found in Transaction reducer.`);
+        }
+}
+
+export default TransactionReducer;
